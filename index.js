debf1f3356a7da54fed447ab1f90c1a96a5f95b3
MG-LS <gggggg1.97@mail.ru>
2022-05-18 15:24:14 +0300

    backkk

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(require("./routes/users.route"));
 app.use(require("./routes/blogs.route"));
+app.use(require("./routes/comments.route"));
 
 app.use(errorMiddleware);
 app.use("/image", express.static(path.resolve(__dirname, "image")));
